8bae62eb06a56ad10703f8e3168665f40bfff9ba
dainon <dainonw@gmail.com>
2013-12-22 23:18:55 -0500

    Added stuff

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :image, :styles => { :large => "600x600>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   #has_attached_file :image, :styles => { :medium => "x300", :thumb => "x100" }
   #has_attached_file :image
 
